14c7d32748598a69737c1f1390825c8996832948
Le Labousse Dorian <dorian.lelabousse@orange.fr>
2021-04-09 12:09:45 +0200

    Control
    Joystick same speed as keyboard
    players cannot unplug their controller

--- test_control.py
+++ test_control.py
@@ -35,32 +35,46 @@ class Player(pygame.sprite.Sprite):
         self.surf = pygame.Surface((75, 25))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
-        self._motion = [0,0]
+        self._motion = [0,0]    #Variable des sticks, motion [0] = axisX
+        self._vitesse = 3
 
-    def changeColor(self, color):
+    def changeColor(self, color):   #Change la couleur du joueur, sert de test pour les boutons des manettes
         self.surf.fill((color))
 
-    def movementKeyboard(self, pressed_keys, up, down, left, right):
+    def movementKeyboard(self, pressed_keys, up, down, left, right):    #Déplace le joueur en fonction des touches clavier
         if pressed_keys[up]:
-            self.rect.move_ip(0, -3)
-        if pressed_keys[down]:
-            self.rect.move_ip(0, 3)
-        if pressed_keys[left]:
-            self.rect.move_ip(-3, 0)
-        if pressed_keys[right]:
-            self.rect.move_ip(3, 0)
+            self.rect.move_ip(0, -self._vitesse)
 
-    def movementJoystick(self):
-        if self._motion[0] > 0.15:
-            self.rect.move_ip(3, 0)
-        if self._motion[0] < -0.15:
-            self.rect.move_ip(-3, 0)
-        if self._motion[1] > 0.15:
-            self.rect.move_ip(0, 3)
-        if self._motion[1] < -0.15:
-            self.rect.move_ip(0, -3) 
+        elif pressed_keys[down]:
+            self.rect.move_ip(0, self._vitesse)
 
-    def setMotion(self, axis, value):
+        elif pressed_keys[left]:
+            self.rect.move_ip(-self._vitesse, 0)
+
+        elif pressed_keys[right]:
+            self.rect.move_ip(self._vitesse, 0)
+
+    def movementJoystick(self): #Bouge le perso en fonction du stick
+            
+        if abs(self._motion[0]) > abs(self._motion[1]): #Empêche le joueur de se déplacer en diagonale
+            self._motion[1] = 0
+
+        else:
+            self._motion[0] = 0
+
+        if self._motion[0] > 0.25:
+            self.rect.move_ip(self._vitesse, 0)
+
+        if self._motion[0] < -0.25:
+            self.rect.move_ip(-self._vitesse, 0)
+
+        if self._motion[1] > 0.25:
+            self.rect.move_ip(0, self._vitesse)
+
+        if self._motion[1] < -0.25:
+            self.rect.move_ip(0, -self._vitesse) 
+
+    def setMotion(self, axis, value):   #_motion récupère l'axe et la valeur du stick
         self._motion[axis] = value
 
 
@@ -73,10 +87,10 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 # Instantiate player. Right now, this is just a rectangle.
-player1 = Player()
+player1 = Player()  #Initialisation des joueurs
 player2 = Player()
 
-players = [player1, player2]
+players = [player1, player2]    #Les joueurs sont placé dans une liste
 
 pygame.joystick.init()
 joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
@@ -93,19 +107,20 @@ while running:
     for event in pygame.event.get():
         # Check for KEYDOWN event
 
-        if event.type == JOYBUTTONDOWN:
+        if event.type == JOYBUTTONDOWN: #Si le joueur appuie sur un boutton d'une manette
             print(event)
 
             if event.button == 0 and event.instance_id == 0:
-                player1.changeColor((100,100,100))
+                player1.changeColor((200,100,200))
+
             if event.button == 0 and event.instance_id == 1:
                 player2.changeColor((200,100,100))
 
 
-        if event.type == JOYAXISMOTION:
+        if event.type == JOYAXISMOTION: #Si unstick d'une manette bouge
             print(event)
-            if event.axis < 2 and event.instance_id < len(players):
-                players[event.instance_id].setMotion(event.axis, event.value)
+            if event.axis < 2 and event.instance_id < len(players): #on vérifie que c'est le stick gauche et que le numéro de la manette correspond a l'un des joueurs
+                players[event.instance_id].setMotion(event.axis, event.value)   #Le joueur correspondant voit son attribut _motion prendre la valeur du stick
 
 
         if event.type == JOYDEVICEADDED:
@@ -126,11 +141,11 @@ while running:
     pressed_keys = pygame.key.get_pressed()
 
 
-    player1.movementKeyboard(pressed_keys, K_UP, K_DOWN, K_LEFT, K_RIGHT)
-    player1.movementJoystick()
+    players[0].movementKeyboard(pressed_keys, K_UP, K_DOWN, K_LEFT, K_RIGHT)
+    players[0].movementJoystick()
 
-    player2.movementKeyboard(pressed_keys, K_z, K_s, K_q, K_d)
-    player2.movementJoystick()
+    players[1].movementKeyboard(pressed_keys, K_z, K_s, K_q, K_d)
+    players[1].movementJoystick()
 
 
     # Fill the screen with white
